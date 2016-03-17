80e48146b3d5bf3fe592113c4a3432631ddfaef7
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-03-17 23:04:55 +0100

    Fix last dep.

--- app/src/main/java/com/mounacheikhna/reactiveapp/base/AppDep.java
+++ app/src/main/java/com/mounacheikhna/reactiveapp/base/AppDep.java
@@ -3,6 +3,7 @@ package com.mounacheikhna.reactiveapp.base;
 import android.app.Application;
 import android.content.Context;
 import com.mounacheikhna.reactiveapp.annotation.ApplicationContext;
+import com.mounacheikhna.reactiveapp.api.geonames.GeonamesApi;
 
 /**
  * Created by cheikhnamouna on 3/17/16.
