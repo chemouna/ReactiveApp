e279ef16bab2812871939c78132b4c05bc41761e
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-03-19 06:50:44 +0100

    Add retrolambda.

--- app/src/main/java/com/mounacheikhna/reactiveapp/MainActivity.java
+++ app/src/main/java/com/mounacheikhna/reactiveapp/MainActivity.java
@@ -17,22 +17,18 @@ public class MainActivity extends AppCompatActivity {
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-
     ButterKnife.bind(this);
-
     Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
   }
 
   @Override public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
     getMenuInflater().inflate(R.menu.menu_main, menu);
     return true;
   }
 
   @Override public boolean onOptionsItemSelected(MenuItem item) {
     int id = item.getItemId();
-
     //noinspection SimplifiableIfStatement
     if (id == R.id.action_settings) {
       return true;
