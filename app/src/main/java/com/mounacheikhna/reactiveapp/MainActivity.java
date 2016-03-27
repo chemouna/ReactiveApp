2e566b072168dd052e25f49ce595eccf1a1d50fa
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-03-28 01:03:57 +0200

    Add drawer

--- app/src/main/java/com/mounacheikhna/reactiveapp/MainActivity.java
+++ app/src/main/java/com/mounacheikhna/reactiveapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.mounacheikhna.reactiveapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -10,6 +11,12 @@ import android.widget.RelativeLayout;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
 import com.mounacheikhna.reactiveapp.ui.Post.PostActivity;
 import com.mounacheikhna.reactiveapp.ui.trips.TripsView;
 
@@ -24,6 +31,27 @@ public class MainActivity extends AppCompatActivity {
     ButterKnife.bind(this);
     Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
+
+    AccountHeader headerResult = new AccountHeaderBuilder()
+        .withActivity(this)
+        .withHeaderBackground(R.drawable.header)
+        .addProfiles(
+            new ProfileDrawerItem().withName("Leila").withEmail("example@gmail.com")
+                .withIcon(ContextCompat.getDrawable(this, R.drawable.profile))
+        )
+        .withOnAccountHeaderListener((view, profile, currentProfile) -> false)
+        .build();
+
+    new DrawerBuilder()
+        .withActivity(this)
+        .withToolbar(toolbar)
+        .withAccountHeader(headerResult)
+        .addDrawerItems(
+            new PrimaryDrawerItem().withName(R.string.search),
+            new DividerDrawerItem(),
+            new PrimaryDrawerItem().withName(R.string.post)
+        )
+        .build();
 
     //TODO: display only when on main screen
     tripsView.displayDefault();
